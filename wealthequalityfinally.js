aed63aefeb3c0d52e7d65aa418181669e4a4d38a
Florian Houenou <88054514+enFloo@users.noreply.github.com>
2022-07-26 23:46:56 -0500

    getting rid of console.log line

--- wealthequalityfinally.js
+++ wealthequalityfinally.js
@@ -1,7 +1,7 @@
 function redistributeWealth(wealth) {
 
     let sum=wealth.reduce((a,b)=>a+b,0)/wealth.length
-    console.log(sum);
+    
     for(var i = 0; i < wealth.length; i++) {
      wealth[i] = sum;
 
